fb97c36c237f6742e9e8e641886d1e4d1382aa12
Yvette Nartey <newerkey@gmail.com>
2022-11-19 01:33:58 +0100

    feat(get-cars): create a path operation to get all cars

--- carsharing.py
+++ carsharing.py
@@ -1,9 +1,41 @@
-from fastapi import FastAPI
+from typing import List, Optional
+
+import uvicorn
+from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
 
+db = [
+    {"id": 1, "size": "s", "fuel": "gasoline", "doors": 3, "transmission": "auto"},
+    {"id": 2, "size": "s", "fuel": "electric", "doors": 3, "transmission": "auto"},
+    {"id": 3, "size": "s", "fuel": "gasoline", "doors": 5, "transmission": "manual"},
+    {"id": 4, "size": "m", "fuel": "electric", "doors": 3, "transmission": "auto"},
+    {"id": 5, "size": "m", "fuel": "hybrid", "doors": 5, "transmission": "auto"},
+    {"id": 6, "size": "m", "fuel": "gasoline", "doors": 5, "transmission": "manual"},
+    {"id": 7, "size": "l", "fuel": "diesel", "doors": 5, "transmission": "manual"},
+    {"id": 8, "size": "l", "fuel": "electric", "doors": 5, "transmission": "auto"},
+    {"id": 9, "size": "l", "fuel": "hybrid", "doors": 5, "transmission": "auto"}
+]
 
-@app.get("/")
-def welcome():
-    """Return a message"""
-    return {'message': "Welcome to the Car Sharing Hub!"}
+
+@app.get("/cars")
+def all_cars(size: str | None = None, doors: int | None = None) -> list:
+    result = db
+    if size:
+        result = [car for car in result if car ['size'] == size]
+    if doors:
+        result = [car for car in result if car ['doors'] >= doors]
+    return result
+
+
+#@app.get("/api/cars/{id}")
+#def car_by_id(id: int) -> dict:
+   # result = [car for car in db if car['id'] == id]
+    #if result:
+       # return result[0]
+    #else:
+       # raise HTTPException(status_code=404, detail=f"No car with id={id}.")
+
+
+#if __name__ == "__main__":
+   # uvicorn.run("carsharing:app", reload=True)
